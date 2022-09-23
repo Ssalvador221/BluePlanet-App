038b001168d24db755c41d9817ae5d691cc387e0
Ssalvador221 <j8731615@gmail.com>
2022-09-22 21:00:26 -0300

    Firebase

--- app/src/main/java/com/example/saaplication/LoginScreen.java
+++ app/src/main/java/com/example/saaplication/LoginScreen.java
@@ -3,12 +3,26 @@ package com.example.saaplication;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.Window;
+import android.view.WindowManager;
+
+import com.google.firebase.auth.FirebaseAuth;
 
 public class LoginScreen extends AppCompatActivity {
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,WindowManager.LayoutParams.FLAG_FULLSCREEN);
         setContentView(R.layout.activity_login_screen);
+
+
+
+
+
+
     }
 }
--- app/src/main/java/com/example/saaplication/RegistroScreen.java
+++ app/src/main/java/com/example/saaplication/RegistroScreen.java
@@ -1,5 +1,6 @@
 package com.example.saaplication;
 
+import android.content.Intent;
 import android.graphics.Color;
 import android.os.Bundle;
 import android.text.TextUtils;
@@ -8,27 +9,43 @@ import android.util.TypedValue;
 import android.view.View;
 import android.view.Window;
 import android.view.WindowManager;
+import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Switch;
 import android.widget.TextView;
+import android.widget.Toast;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import com.example.saaplication.databinding.ActivityRegistroScrenBinding;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.Task;
 import com.google.android.material.snackbar.Snackbar;
+import com.google.android.material.textfield.TextInputEditText;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseAuthWeakPasswordException;
 
 import org.w3c.dom.Text;
+
+import javax.annotation.meta.When;
 
 public class RegistroScreen extends AppCompatActivity {
 
     private ActivityRegistroScrenBinding binding;
+    FirebaseAuth auth;
 
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        binding = ActivityRegistroScrenBinding.inflate(getLayoutInflater());
         requestWindowFeature(Window.FEATURE_NO_TITLE);
+        binding = ActivityRegistroScrenBinding.inflate(getLayoutInflater());
         getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        setContentView(R.layout.activity_registro_scren);
+
         setContentView(binding.getRoot());
 
 
@@ -40,17 +57,40 @@ public class RegistroScreen extends AppCompatActivity {
                 EditText email = binding.emailText;
                 EditText senha = binding.senhaText;
 
+                auth = FirebaseAuth.getInstance();
+
                 if (TextUtils.isEmpty(nome.getText().toString()) || TextUtils.isEmpty(email.getText().toString()) || TextUtils.isEmpty(senha.getText().toString())){
                     Snackbar mySnackbar = Snackbar.make(v, "Preencha todos os Campos!", Snackbar.LENGTH_SHORT);
                     mySnackbar.getView().setBackgroundColor(Color.parseColor("#ffa500"));
                     mySnackbar.setTextColor(Color.parseColor("#ffffff"));
                     mySnackbar.show();
                 }else{
-
+                        auth.createUserWithEmailAndPassword(email.getText().toString(), senha.getText().toString()).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                            @Override
+                            public void onComplete(@NonNull Task<AuthResult> task) {
+                                if (task.isSuccessful()) {
+                                    Snackbar mySnackbar = Snackbar.make(v, "Sucesso ao Cadastrar o Usuário!", Snackbar.LENGTH_SHORT);
+                                    mySnackbar.getView().setBackgroundColor(Color.parseColor("#ffa500"));
+                                    mySnackbar.setTextColor(Color.parseColor("#ffffff"));
+                                    mySnackbar.show();
+                                    binding.nameBox.setText("");
+                                    binding.emailText.setText("");
+                                    binding.senhaText.setText("");
+                                }
+                            }
+                        }).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                            @Override
+                            public void onComplete(@NonNull Task<AuthResult> task) {
+                                switch (task){
+                                    case task:
+                                }
+                            }
+                        });
                 }
             }
         });
 
-
+        
     }
+ 
 }
