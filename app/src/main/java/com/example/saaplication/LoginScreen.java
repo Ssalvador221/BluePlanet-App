731ff3457d17fae44af86b909f481211b2918667
JOÃO SALVADOR RIZZO <joao_rizzo@estudante.sesisenai.org.br>
2022-09-27 12:47:23 -0300

    Continuar em casa

--- app/src/main/java/com/example/saaplication/LoginScreen.java
+++ app/src/main/java/com/example/saaplication/LoginScreen.java
@@ -1,85 +1,33 @@
 
 package com.example.saaplication;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
+import android.view.View;
 import android.view.Window;
 import android.view.WindowManager;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ProgressBar;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.android.material.snackbar.Snackbar;
+import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
 
 public class LoginScreen extends AppCompatActivity {
 
-    private EditText etEmail;
-    private EditText etSenha;
-    private Button btCadastrar;
-    private ProgessBar progessBar;
-    private FirebaseAuth mAuth;
-    String[] mensagens = {"Preencha todos os campos","Login efetuado com sucesso"}
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        requestWindowFeature(Window.FEATURE_NO_TITLE);
-        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,WindowManager.LayoutParams.FLAG_FULLSCREEN);
         setContentView(R.layout.activity_login_screen);
 
-        getSupportActionBar().hide();
-        IniciarComponentes();
     }
 
-    btCadastrar.setOnClickListener(new View.OnClickListener() {
-        @Override
-        public void onClick(View v) {
 
-            String email = etEmail.getText().toString();
-            String senha = etSenha.getText().toString();
-
-            if (email.isEmpty() || senha.isEmpty()) {
-                Snackbar snackbar = Snackbar.make(v, mensagens[0], Snackbar.LENGHT_SHORT);
-                snackbar.setBackgroundTint("#ffa500");
-                snackbar.setTextColor("#ffffff");
-                snackbar.show();
-             }else{
-                AutenticarUsuario()
-            }
-        });
-    }
-
-    private void AutenticarUsuario(){
-
-        String email = etEmail.getText().toString();
-        String senha = etSenha.getText().toString();
-
-        FirebaseAuth.getInstance().signInWithEmailAndPassword(email,senha).addOncompleteListener(new OnCompleteListener<AuthResult>() {
-            @Override
-            public void onComplete(@NonNull Task<AuthResult> task) {
-
-                if (task.isSuccessful()){
-                    progessBar.setVisibility(View.VISIBLE);
-
-                    new Handler().postDelayed(new Runnable() {
-                        @Override
-                        public void run() {
-                            Home();
-                        }
-                    }, 5000)
-                }
-            }
-        });
-    }
-
-    private void Home(){
-        Intent intent = new Intent(RegistroScreen.this,Home.class);
-        startActivity(intent);
-        finish();
-    }
-
-    private void IniciarComponentes(){
-        etEmail = findViewById(R.id.etEmail);
-        etSenha = findViewById(R.id.etSenha);
-        btCadastrar = findViewById(R.id.btcadastrar)
-        progessBar = findViewById(R.id.progressbar)
-    }
 }
--- app/src/main/java/com/example/saaplication/RegistroScreen.java
+++ app/src/main/java/com/example/saaplication/RegistroScreen.java
@@ -1,109 +1,86 @@
 package com.example.saaplication;
 
-import android.content.Intent;
+
 import android.graphics.Color;
-import android.nfc.Tag;
+
 import android.os.Bundle;
-import android.text.TextUtils;
-import android.util.Log;
-import android.util.Size;
-import android.util.TypedValue;
+
+
 import android.view.View;
-import android.view.Window;
-import android.view.WindowManager;
 import android.widget.Button;
 import android.widget.EditText;
-import android.widget.Switch;
-import android.widget.TextView;
-import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
-import com.example.saaplication.databinding.ActivityRegistroScrenBinding;
-import com.example.saaplication.modelo.Usuario;
 import com.google.android.gms.tasks.OnCompleteListener;
-import com.google.android.gms.tasks.OnFailureListener;
 import com.google.android.gms.tasks.Task;
 import com.google.android.material.snackbar.Snackbar;
-import com.google.android.material.textfield.TextInputEditText;
 import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
-import com.google.firebase.auth.FirebaseAuthEmailException;
-import com.google.firebase.auth.FirebaseAuthUserCollisionException;
-import com.google.firebase.auth.FirebaseAuthWeakPasswordException;
-import com.google.firebase.auth.FirebaseUser;
-import com.google.firebase.firestore.auth.User;
 
-import org.checkerframework.checker.guieffect.qual.UI;
-import org.w3c.dom.Text;
-
-import java.nio.charset.StandardCharsets;
-
-import javax.annotation.meta.When;
 
 public class RegistroScreen extends AppCompatActivity {
 
-    private EditText etNome;
-    private EditText etEmail;
-    private EditText etSenha;
-    private Button btCadastrar;
-    private FirebaseAuth mAuth;
-    private Usuario u;
+    private EditText namebox, emaibox, senhabox;
+    private Button btcadastrar;
+    FirebaseAuth auth;
+    String[] mensagens = {"Preencha todos os Campos solicitados!", "Usuário Criado com Sucesso!"};
+
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
+    protected void onCreate(Bundle savedIntanceState) {
+        super.onCreate(savedIntanceState);
         setContentView(R.layout.activity_registro_scren);
-        etNome = findViewById(R.id.name_box);
-        etEmail = findViewById(R.id.email_text);
-        etSenha = findViewById(R.id.senha_text);
-        btCadastrar = findViewById(R.id.bt_registrar);
-        mAuth = FirebaseAuth.getInstance();
 
-        btCadastrar.setOnClickListener(new View.OnClickListener() {
+        IniciarComponentes();
+
+        btcadastrar.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onClick(View v) {
-                recuperarDados();
-                criarLogin();
+            public void onClick(View view) {
+                String nome = namebox.getText().toString();
+                String email = emaibox.getText().toString();
+                String senha = senhabox.getText().toString();
+
+                if (nome.isEmpty() || email.isEmpty() || senha.isEmpty()) {
+                    Snackbar mysnack = Snackbar.make(view, mensagens[0], Snackbar.LENGTH_SHORT);
+                    mysnack.getView().setBackgroundColor(Color.parseColor("#ffa500"));
+                    mysnack.setTextColor(Color.parseColor("#ffffff"));
+                    mysnack.show();
+                } else {
+                    cadastrarUsuario(view);
+                }
             }
         });
-
-
     }
 
-    private void criarLogin() {
-        mAuth.createUserWithEmailAndPassword(u.getEmail(),u.getSenha())
-                .addOnCompleteListener(new OnCompleteListener<AuthResult>() {
-                    @Override
-                    public void onComplete(@NonNull Task<AuthResult> task) {
-                        if(task.isSuccessful()){
-                            FirebaseUser user = mAuth.getCurrentUser();
-                            u.setId(user.getUid());
-                            u.salvarDados();
-                        }else{
-                            Snackbar mySnackbar = Snackbar.make(btCadastrar, "Preencha todos os Campos!", Snackbar.LENGTH_SHORT);
-                            mySnackbar.getView().setBackgroundColor(Color.parseColor("#ffa500"));
-                            mySnackbar.setTextColor(Color.parseColor("#ffffff"));
-                            mySnackbar.show();
-                        }
-                    }
-                });
+    private void cadastrarUsuario(View view) {
+
+        auth = FirebaseAuth.getInstance();
+
+        String email = emaibox.getText().toString();
+        String senha = senhabox.getText().toString();
+
+        auth.createUserWithEmailAndPassword(email,senha).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+            @Override
+            public void onComplete(@NonNull Task<AuthResult> task) {
+                if (task.isSuccessful()){
+                    Snackbar mysnack = Snackbar.make(view, mensagens[0], Snackbar.LENGTH_SHORT);
+                    mysnack.getView().setBackgroundColor(Color.parseColor("#ffa500"));
+                    mysnack.setTextColor(Color.parseColor("#ffffff"));
+                    mysnack.show();
+                }
+
+            }
+        });
     }
 
+    private void IniciarComponentes() {
 
+        namebox = findViewById(R.id.name_box);
+        emaibox = findViewById(R.id.email_text);
+        senhabox = findViewById(R.id.senha_text);
+        btcadastrar = findViewById(R.id.bt_registrar);
 
-    private void recuperarDados() {
-        if(etNome.getText().toString()==""||etEmail.getText().toString()==""||etSenha.getText().toString()==""){
-            Toast.makeText(RegistroScreen.this,"Você deve preencher todos os Dados corretamente!",Toast.LENGTH_LONG);
-        }else{
-            u = new Usuario();
-            u.setNome(etNome.getText().toString());
-            u.setEmail(etEmail.getText().toString());
-           u.setSenha(etSenha.getText().toString());
-        }
     }
 }
-
-
-
