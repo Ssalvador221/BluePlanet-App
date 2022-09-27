e3846456e58cae894c7b9e381f5be08adb313e11
erick_bertoti <erick_bertoti@estudante.sc.senai.br>
2022-09-27 08:41:10 -0300

    login feito

--- app/src/main/java/com/example/saaplication/LoginScreen.java
+++ app/src/main/java/com/example/saaplication/LoginScreen.java
@@ -10,7 +10,10 @@ import com.google.firebase.auth.FirebaseAuth;
 
 public class LoginScreen extends AppCompatActivity {
 
-
+    private EditText etEmail;
+    private EditText etSenha;
+    private Button btCadastrar;
+    private FirebaseAuth mAuth;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -18,11 +21,43 @@ public class LoginScreen extends AppCompatActivity {
         requestWindowFeature(Window.FEATURE_NO_TITLE);
         getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,WindowManager.LayoutParams.FLAG_FULLSCREEN);
         setContentView(R.layout.activity_login_screen);
+        mAuth = FirebaseAuth.getInstance();
+        etEmail = findViewById(R.id.etEmail);
+        etSenha = findViewById(R.id.etSenha);
+        btCadastrar = findById(R.id.btLogar);
 
+        btCadastrar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                receberDados();
+                logar();
+            }
+        });
+    }
 
+    private void logar() {
+        mAuth.signInWithEmailAndPassword(email, password)
+                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if (task.isSuccessful()) {
+                            FirebaseUser user = mAuth.getCurrentUser();
+                            startActivity(new Intent(LoginScreen.this,Home.class));
 
+                        } else {
 
+                            Toast.makeText(LoginScreen.this, "Autenticação falhou",
+                                    Toast.LENGTH_SHORT).show();
+                        }
+                    }
+                });
+    }
 
+    private void receberDados() {
+
+        u = new Usuario();
+        u.setEmail(etEmail.getText().toString());
+        u.setSenha(etSenha.getText().toString());
 
     }
 }
