a8ec549f6a18ec72792bd2894ec5e5a26c74cce5
Ssalvador221 <j8731615@gmail.com>
2022-09-28 23:55:32 -0300

    Continuar amanha

--- app/src/main/java/com/example/saaplication/LoginScreen.java
+++ app/src/main/java/com/example/saaplication/LoginScreen.java
@@ -1,33 +1,57 @@
-
 package com.example.saaplication;
 
-import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.content.Intent;
+import android.graphics.Color;
 import android.os.Bundle;
-import android.os.Handler;
 import android.view.View;
-import android.view.Window;
-import android.view.WindowManager;
 import android.widget.Button;
 import android.widget.EditText;
-import android.widget.ProgressBar;
 
-import com.google.android.gms.tasks.OnCompleteListener;
-import com.google.android.gms.tasks.Task;
+import com.google.android.material.snackbar.BaseTransientBottomBar;
 import com.google.android.material.snackbar.Snackbar;
-import com.google.firebase.auth.AuthResult;
-import com.google.firebase.auth.FirebaseAuth;
 
 public class LoginScreen extends AppCompatActivity {
+
+    private  EditText box_email, senhabox;
+    private Button btLogin;
+    //private ProgressBar progressBar;
+    String[] mensagem = {"Preencha todos os Campos!", "Login efetuado com sucesso!"};
+
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login_screen);
 
+        IniciarComponentes();
+
+
+        btLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                String email = box_email.getText().toString();
+                String senha  = senhabox.getText().toString();
+
+                if (email.isEmpty() || senha.isEmpty()){
+
+                    Snackbar snackbar = Snackbar.make(view, mensagem[0], BaseTransientBottomBar.LENGTH_SHORT);
+                    snackbar.getView().setBackgroundColor(Color.parseColor("#ffffff"));
+                    snackbar.setTextColor(Color.parseColor("#000000"));
+                    snackbar.show();
+                }
+            }
+        });
+
     }
 
 
+    private void IniciarComponentes() {
+        box_email = findViewById(R.id.box_email);
+        senhabox = findViewById(R.id.senhabox);
+        btLogin = findViewById(R.id.btLogin);
+    }
 }
---
+++ app/src/main/java/com/example/saaplication/PaginaInicial.java
@@ -0,0 +1,14 @@
+package com.example.saaplication;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class PaginaInicial extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_pagina_inicial);
+    }
+}
